2622b8ed1d1147e31d6d9cddfa864cfbf6ad4f03
Hiroki Kajiwara <rerun.0510@gmail.com>
2022-11-01 18:20:35 +0900

    add chakra ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,26 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import {
+  ChakraProvider,
+  extendTheme,
+} from '@chakra-ui/react'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const colors = {
+  brand: {
+    900: '#1a365d',
+    800: '#153e75',
+    700: '#2a69ac',
+  },
+}
+
+const theme = extendTheme({ colors })
+
+export default function App({
+  Component,
+  pageProps,
+}: AppProps) {
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
