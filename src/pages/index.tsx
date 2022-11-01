5d2696b33513e12bf71db68cf388371d17367d12
Hiroki Kajiwara <rerun.0510@gmail.com>
2022-11-01 18:38:02 +0900

    pages directory changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,21 @@
 import { Flex, Box, Circle } from '@chakra-ui/react'
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
 
 export default function Home() {
+  const router = useRouter()
+  const [loading, setLoading] = useState<boolean>(true)
+
+  useEffect(() => {
+    if (router.isReady) {
+      setLoading(false)
+    }
+  }, [router.isReady])
+
+  if (loading) return <></>
+
   return (
-    <>
+    <Box>
       <Flex>
         <Box h="5rem" w="5rem" bg="red" />
         <Box h="5rem" w="5rem" bg="blue" />
@@ -14,6 +27,6 @@ export default function Home() {
         <Box h="5rem" w="5rem" bg="red" />
       </Flex>
       <Circle size="5rem" bg="yellow" />
-    </>
+    </Box>
   )
 }
